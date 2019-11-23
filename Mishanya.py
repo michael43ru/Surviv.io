b2d1f77fc6e312ddd8e99d7880b73c65d7d59620
michael43ru <55547179+michael43ru@users.noreply.github.com>
2019-11-23 14:59:35 +0300

    Add files via upload
    popbItka sdelat' cameru i krasnuyu zonu

--- Mishanya.py
+++ Mishanya.py
@@ -1,5 +1,13 @@
-import pygame
+import pygame as pg
+
 import random
+
+import time
+
+from random import randrange as rnd, choice
+
+from os import path
+
 
 width = 360
 height = 480
@@ -11,32 +19,74 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
-color_trava = (160, 255, 100)
-color_voda = (70, 120, 255)
+color_trava = [160, 255, 100]
+color_voda = [70, 120, 255]
+
 
 # Создаем игру и окно
-pygame.init()
-pygame.mixer.init() # Звук
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+pg.init()
+pg.mixer.init() # Звук
+screen = pg.display.set_mode((width, height))
+pg.display.set_caption("My Game")
+clock = pg.time.Clock()
 
+'''class River:'''
+s = 100000
+'''pole = '''
+
+img_dir = path.join(path.dirname(__file__), 'img')
+
+class Red_zone:
+    def __init__(self):
+        self.x = rnd(0.4 * s, 0.6 * s)
+        self.y = rnd(0.4 * s, 0.6 * s)
+        self.r = 0.4 * s
+        self.t = time.time()
+
+        self.image = pg.image.load(path.join(img_dir, "Red_zone.png")).convert()
+        self.image.set_colorkey(white)
+        self.image_rect = self.image.get_rect()
+
+    def center(self):
+        self.r = self.r / 2
+        self.x = rnd(-self.r + self.x, self.r + self.x)
+        self.y = rnd(-(self.r ** 2 - self.x ** 2) + self.y,
+                     (self.r ** 2 - self.x ** 2) + self.y)
+        self.R = self.r * 3
+        
+    def reduction(self):
+        self.R -= self.v
+        
+
+
+# камера
+'''for a in static_objects:
+    for b in a:
+        b.x -= player.vx
+        b.y -= player.vy
+for a in dinamic_objects:
+    for b in a:
+        b.x -= player.vx
+        b.y -= player.vy'''
+
+zone = Red_zone()
 # Цикл игры
 running = True
 while running:
     # Держим цикл на правильной скорости
     clock.tick(fps)
     # Ввод процесса (события)
-    for event in pygame.event.get():
+    for event in pg.event.get():
         # check for closing window
-        if event.type == pygame.QUIT:
+        if event.type == pg.QUIT:
             running = False
 
     # Обновление
     
     # Рендеринг
-    screen.fill(color_voda)
+    screen.fill(color_trava)
+    screen.blit(zone.image, zone.image_rect)
     # После отрисовки всего, переворачиваем экран
-    pygame.display.flip()
+    pg.display.flip()
 
-pygame.quit()
+pg.quit()
