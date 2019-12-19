19f9241a3f4cfff1b523749d9db6f5384a8ff3bd
zykovao132001 <55546860+zykovao132001@users.noreply.github.com>
2019-12-19 05:30:41 +0300

    Update RedZone.py

--- RedZone.py
+++ RedZone.py
@@ -1,21 +1,17 @@
+import pygame
+import math
+
+
 class Red_zone:
-    def __init__(self):
-        self.x = rnd(0.3 * s, 0.7 * s)
-        self.y = rnd(0.3 * s, 0.5 * s)
-        self.r = 0.5 * s
-        self.t = time.time()
+    def __init__(self, x, y , r, sc):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.v = 5
 
-        # self.image = pg.image.load(path.join(img_dir, "Red_zone.png")).convert()
-        self.image = pg.transform.scale(red_zone_img, (1000, 1000))
-        self.image.set_colorkey(white)
-        self.image_rect = self.image.get_rect()
+    def draw(self):
+        self.image = pygame.draw.circle(sc, (100, 0, 0), (self.x, self.y), self.r, 10)
 
-    def get_center(self):
-        self.r = self.r / 2
-        self.x = rnd(-self.r + self.x, self.r + self.x)
-        self.y = rnd(-(self.r ** 2 - self.x ** 2) + self.y,
-                     (self.r ** 2 - self.x ** 2) + self.y)
-        self.R = self.r * 3
-
-    def reduction(self): # Не доделано
-        self.R -= self.v
+    def hit(self, obj):
+        if math.hypot((self.x - obj.x), (self.y - obj.y)) <= self.r:
+            obj.health -= 0.5
